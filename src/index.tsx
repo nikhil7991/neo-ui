17c7bc66eb6f4f587a713c23017500d87d6d6542
Nikhil Negi <nikhil.negi7991@gmail.com>
2022-07-14 16:54:52 +0530

    v1.0.8

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,3 @@
-export { Input, Checkbox, Select, TextArea, MultiSelect, Radio } from './components/Forms/Inputs/index';
+import { Input, Checkbox, Select, TextArea, MultiSelect, Radio } from './components/Forms/Inputs/index';
+
+export { Input, Checkbox, Select, TextArea, MultiSelect, Radio };
